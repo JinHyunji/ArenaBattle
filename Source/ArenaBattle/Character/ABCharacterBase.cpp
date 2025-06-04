4af405dcf0917e4bb12c34ed3e8f6ecda7de1f89
진현지 <jinhyunji520@gmail.com>
2025-06-04 14:36:36 +0900

    Add Combo Attack and Hp bar

--- Source/ArenaBattle/Character/ABCharacterBase.cpp
+++ Source/ArenaBattle/Character/ABCharacterBase.cpp
@@ -7,6 +7,10 @@
 #include "GameFramework/CharacterMovementComponent.h"
 #include "ABCharacterControlData.h"
 #include "Animation/AnimMontage.h"
+#include "ABComboActionData.h"
+#include "Physics/ABCollision.h"
+#include "Engine/DamageEvents.h"
+#include "ABCharacterNonPlayer.h"
 
 // Sets default values
 AABCharacterBase::AABCharacterBase()
@@ -18,7 +22,7 @@ AABCharacterBase::AABCharacterBase()
 
     // Capsule
     GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
-    GetCapsuleComponent()->SetCollisionProfileName(TEXT("Pawn"));
+    GetCapsuleComponent()->SetCollisionProfileName(CPROFILE_ABCAPSULE);
 
     // Movement
     GetCharacterMovement()->bOrientRotationToMovement = true;
@@ -32,7 +36,7 @@ AABCharacterBase::AABCharacterBase()
     // Mesh
     GetMesh()->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, -100.0f), FRotator(0.0f, -90.0f, 0.0f));
     GetMesh()->SetAnimationMode(EAnimationMode::AnimationBlueprint);
-    GetMesh()->SetCollisionProfileName(TEXT("CharacterMesh"));
+    GetMesh()->SetCollisionProfileName("NoCollision");
 
     static ConstructorHelpers::FObjectFinder<USkeletalMesh> CharacterMeshRef(TEXT("/Script/Engine.SkeletalMesh'/Game/InfinityBladeWarriors/Character/CompleteCharacters/SK_CharM_Cardboard.SK_CharM_Cardboard'"));
     if (CharacterMeshRef.Object)
@@ -58,6 +62,23 @@ AABCharacterBase::AABCharacterBase()
         CharacterControlManager.Add(ECharacterControlType::Quater, QuaterDataRef.Object);
     }
 
+    static ConstructorHelpers::FObjectFinder<UAnimMontage> ComboActionMontageRef(TEXT("/Script/Engine.AnimMontage'/Game/ArenaBattle/Animation/AM_ComboAttack.AM_ComboAttack'"));
+    if (ComboActionMontageRef.Object)
+    {
+        ComboActionMontage = ComboActionMontageRef.Object;
+    }
+
+    static ConstructorHelpers::FObjectFinder<UABComboActionData> ComboActionDataRef(TEXT("/Script/ArenaBattle.ABComboActionData'/Game/ArenaBattle/CharacterAction/ABA_ComboAttack.ABA_ComboAttack'"));
+    if (ComboActionDataRef.Object)
+    {
+        ComboActionData = ComboActionDataRef.Object;
+    }
+
+    static ConstructorHelpers::FObjectFinder<UAnimMontage> DeadMontageRef(TEXT("/Script/Engine.AnimMontage'/Game/ArenaBattle/Animation/AM_Dead.AM_Dead'"));
+    if (DeadMontageRef.Object)
+    {
+        DeadMontage = DeadMontageRef.Object;
+    }
 }
 
 void AABCharacterBase::SetCharacterControlData(const UABCharacterControlData* CharacterControlData)
@@ -76,6 +97,16 @@ void AABCharacterBase::ProcessComboCommand()
     if (CurrentCombo == 0)
     {
         ComboActionBegin();
+        return;
+    }
+
+    if (!ComboTimerHandle.IsValid())
+    {
+        HasNextComboCommand = false;
+    }
+    else
+    {
+        HasNextComboCommand = true;
     }
 }
 
@@ -95,6 +126,9 @@ void AABCharacterBase::ComboActionBegin()
     FOnMontageEnded EndDelegate;
     EndDelegate.BindUObject(this, &AABCharacterBase::ComboActionEnd);
     AnimInstance->Montage_SetEndDelegate(EndDelegate, ComboActionMontage);
+
+    ComboTimerHandle.Invalidate();
+    SetComboCheckTimer();
 }
 
 void AABCharacterBase::ComboActionEnd(UAnimMontage* TargetMontage, bool IsProperlyEnded)
@@ -104,4 +138,82 @@ void AABCharacterBase::ComboActionEnd(UAnimMontage* TargetMontage, bool IsProper
     GetCharacterMovement()->SetMovementMode(EMovementMode::MOVE_Walking);
 }
 
+void AABCharacterBase::SetComboCheckTimer()
+{
+    int32 ComboIndex = CurrentCombo - 1;
+    ensure(ComboActionData->EffectiveFrameCount.IsValidIndex(ComboIndex));
 
+    const float AttackSpeedRate = 1.0f;
+    float ComboEffectiveTime = (ComboActionData->EffectiveFrameCount[ComboIndex] / ComboActionData->FrameRate) / AttackSpeedRate;
+    if (ComboEffectiveTime > 0.0f)
+    {
+        GetWorld()->GetTimerManager().SetTimer(ComboTimerHandle, this, &AABCharacterBase::ComboCheck, ComboEffectiveTime, false);
+    }
+}
+
+void AABCharacterBase::ComboCheck()
+{
+    ComboTimerHandle.Invalidate();
+    if (HasNextComboCommand)
+    {
+        UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
+
+        CurrentCombo = FMath::Clamp(CurrentCombo + 1, 1, ComboActionData->MaxComboCount);
+        FName NextSection = *FString::Printf(TEXT("%s%d"), *ComboActionData->MontageSectionNamePrefix, CurrentCombo);
+        AnimInstance->Montage_JumpToSection(NextSection, ComboActionMontage);
+        SetComboCheckTimer();
+        HasNextComboCommand = false;
+    }
+}
+
+void AABCharacterBase::AttackHitCheck()
+{
+    FHitResult OutHitResult;
+    FCollisionQueryParams Params(SCENE_QUERY_STAT(Attack), false, this);
+
+    const float AttackRange = 40.0f;
+    const float AttackRadius = 50.0f;
+    const float AttackDamage = 30.0f;
+    const FVector Start = GetActorLocation() + GetActorForwardVector() * GetCapsuleComponent()->GetScaledCapsuleRadius();
+    const FVector End = Start + GetActorForwardVector() * AttackRange;
+
+    bool HitDetected = GetWorld()->SweepSingleByChannel(OutHitResult, Start, End, FQuat::Identity, CCHANNEL_ABACTION, FCollisionShape::MakeSphere(AttackRadius), Params);
+    if (HitDetected)
+    {
+        FDamageEvent DamageEvent;
+        OutHitResult.GetActor()->TakeDamage(AttackDamage, DamageEvent, GetController(), this);
+    }
+
+#if ENABLE_DRAW_DEBUG
+
+    FVector CapsuleOrigin = Start + (End - Start) * 0.5f;
+    float CapsuleHalfHeight = AttackRange * 0.5f;
+    FColor DrawColor = HitDetected ? FColor::Green : FColor::Red;
+
+    DrawDebugCapsule(GetWorld(), CapsuleOrigin, CapsuleHalfHeight, AttackRadius, FRotationMatrix::MakeFromZ(GetActorForwardVector()).ToQuat(), DrawColor, false, 5.0f);
+
+#endif
+}
+
+float AABCharacterBase::TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser)
+{
+    Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+
+    SetDead();
+
+    return DamageAmount;
+}
+
+void AABCharacterBase::SetDead()
+{
+    GetCharacterMovement()->SetMovementMode(EMovementMode::MOVE_None);
+    PlayDeadAnimation();
+    SetActorEnableCollision(false);
+}
+
+void AABCharacterBase::PlayDeadAnimation()
+{
+    UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
+    AnimInstance->StopAllMontages(0.0f);
+    AnimInstance->Montage_Play(DeadMontage, 1.0f);
+}
